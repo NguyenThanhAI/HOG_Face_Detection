65087fdf7d4ea2a18b3e5abc568794cc3ef1c380
NguyenThanhAI <Thanh.CEAA.K58.HUST@gmail.com>
2022-12-29 12:36:11 +0700

    end pipeline

--- anchor_generator.py
+++ anchor_generator.py
@@ -1,18 +1,22 @@
 import os
 from typing import Dict, List, Optional
-
+import joblib
+from joblib import Parallel, delayed
 from tqdm import tqdm
 
 import numpy as np
-from skimage import color
+from skimage import io, color, transform, feature
 import cv2
 
+image_path = r"C:\Users\Thanh_Tuyet\Downloads\practical-face-detection\practical-face-detection\images\img1.jpg"
 
 base_dim: int = 50
-scales = [0.5, 1., 2., 3., 4.]
+scales = [0.25, 0.5, 0.75, 1., 1.25, 1.5, 1.75, 2.]
 ratios = [1, 1.2]
 offset = 0.5
 stride = 0.2
+
+threshold = 0.99
 
 #width = base_dim * scales[:, np.newaxis] / np.sqrt(ratios[np.newaxis, :])
 #height = base_dim * scales[:, np.newaxis] * np.sqrt(ratios[np.newaxis, :])
@@ -30,8 +34,10 @@ stride = 0.2
 #centers = np.meshgrid(y, x)
 #
 #print(centers)
-
-image_size = (500, 500)
+image = io.imread(image_path)
+result_image = image.copy()
+#image_size = (500, 500)
+image_size = tuple(list(image.shape[:2]))
 
 anchors: Dict[float, Dict[float, np.ndarray]] = {}
 
@@ -58,15 +64,16 @@ for scale in scales:
         print(coords)
         anchors[scale][ratio] = coords
         
-image = np.ones(shape=[image_size[0], image_size[1], 3], dtype=np.uint8) * 255
-#for i, rect in enumerate(anchors[1][1.2]):
-#    cv2.rectangle(image, (int(rect[1]), int(rect[0])), (int(rect[3]), int(rect[2])), color=(0, 0, 255), thickness=1)
-#    cv2.putText(image, text=str(i+1), org=(int(rect[1])+10, int(rect[0])+10), fontFace=cv2.FONT_HERSHEY_COMPLEX, fontScale=0.3, color=(0, 255, 0))
-#cv2.imshow("Anh", image)
-#cv2.waitKey(0)
-
+#image = np.ones(shape=[image_size[0], image_size[1], 3], dtype=np.uint8) * 255
+for i, rect in enumerate(anchors[0.75][1]):
+    cv2.rectangle(image, (int(rect[1]), int(rect[0])), (int(rect[3]), int(rect[2])), color=(0, 0, 255), thickness=1)
+    cv2.putText(image, text=str(i+1), org=(int(rect[1])+10, int(rect[0])+10), fontFace=cv2.FONT_HERSHEY_COMPLEX, fontScale=0.3, color=(0, 255, 0))
+cv2.imshow("Anh", image[:, :, ::-1])
+cv2.waitKey(0)
+dict_crop_images = {}
 gray_image = color.rgb2gray(image)
 for scale in anchors.keys():
+    dict_crop_images[scale] = {}
     for ratio in anchors[scale].keys():
         print("Scale: {}, ratio: {}".format(scale, ratio))
         crop_images = []
@@ -74,4 +81,55 @@ for scale in anchors.keys():
             crop_images.append(gray_image[int(rect[0]):int(rect[2]), int(rect[1]):int(rect[3])])
 
         crop_images = np.stack(crop_images, axis=0)
+        dict_crop_images[scale][ratio] = crop_images
         print(crop_images.shape)
+
+hog_features = {}     
+        
+for scale in dict_crop_images.keys():
+    hog_features[scale] = {}
+    for ratio in dict_crop_images[scale].keys():
+        print("Scale: {}, ratio: {}".format(scale, ratio))
+        features = []
+        crop_images = dict_crop_images[scale][ratio]
+        for img in tqdm(crop_images):
+            resized_img = transform.resize(img, output_shape=(24, 24))
+            feat = feature.hog(image=resized_img, orientations=9, cells_per_block=(2, 2), block_norm="L2")
+            features.append(feat)
+        features = np.stack(features, axis=0)
+        print(features.shape)
+        hog_features[scale][ratio] = features
+        
+
+model = joblib.load("svm_model.pkl")
+
+bboxes = []
+probs = {}
+chosen_probs = []
+for scale in hog_features.keys():
+    probs[scale] = {}
+    for ratio in hog_features[scale].keys():
+        print("Scale: {}, ratio: {}".format(scale, ratio))
+        features = hog_features[scale][ratio]
+        predict_prob = model.predict_proba(features)[:, 1]
+        #print(predict_prob.shape)
+        probs[scale][ratio] = predict_prob
+        
+        pos_anchor = np.where(predict_prob > threshold)[0]
+        if pos_anchor.shape[0] > 0:
+            pos_boxes = anchors[scale][ratio][pos_anchor]
+            pos_probs = predict_prob[pos_anchor]
+            bboxes.append(pos_boxes)
+            chosen_probs.append(pos_probs)
+            print(pos_boxes.shape)
+            assert pos_boxes.shape[0] == pos_probs.shape[0]
+            
+bboxes = np.concatenate(bboxes, axis=0)
+chosen_probs = np.concatenate(chosen_probs, axis=0)
+print(bboxes, chosen_probs)
+
+for rect in bboxes:
+    cv2.rectangle(result_image, (int(rect[1]), int(rect[0])), (int(rect[3]), int(rect[2])), color=(0, 0, 255), thickness=1)
+
+cv2.imshow("Anh", result_image[:, :, ::-1])
+cv2.waitKey(0)
